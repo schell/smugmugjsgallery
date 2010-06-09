3ff544f95215bcf04e48de4c4ae8fbd9d369f17d
Schell Scivally <efsubenovex@gmail.com>
2010-06-08 17:16:10 -0700

    first version pretty done

---
+++ smugal/mootools-1.2.4.4-more.js
@@ -0,0 +1,366 @@
+//MooTools More, <http://mootools.net/more>. Copyright (c) 2006-2009 Aaron Newton <http://clientcide.com/>, Valerio Proietti <http://mad4milk.net> & the MooTools team <http://mootools.net/developers>, MIT Style License.
+
+/*
+---
+
+script: More.js
+
+description: MooTools More
+
+license: MIT-style license
+
+authors:
+- Guillermo Rauch
+- Thomas Aylott
+- Scott Kyle
+
+requires:
+- core:1.2.4/MooTools
+
+provides: [MooTools.More]
+
+...
+*/
+
+MooTools.More = {
+	'version': '1.2.4.4',
+	'build': '6f6057dc645fdb7547689183b2311063bd653ddf'
+};
+
+/*
+---
+
+script: Element.Measure.js
+
+description: Extends the Element native object to include methods useful in measuring dimensions.
+
+credits: "Element.measure / .expose methods by Daniel Steigerwald License: MIT-style license. Copyright: Copyright (c) 2008 Daniel Steigerwald, daniel.steigerwald.cz"
+
+license: MIT-style license
+
+authors:
+- Aaron Newton
+
+requires:
+- core:1.2.4/Element.Style
+- core:1.2.4/Element.Dimensions
+- /MooTools.More
+
+provides: [Element.Measure]
+
+...
+*/
+
+Element.implement({
+
+	measure: function(fn){
+		var vis = function(el) {
+			return !!(!el || el.offsetHeight || el.offsetWidth);
+		};
+		if (vis(this)) return fn.apply(this);
+		var parent = this.getParent(),
+			restorers = [],
+			toMeasure = []; 
+		while (!vis(parent) && parent != document.body) {
+			toMeasure.push(parent.expose());
+			parent = parent.getParent();
+		}
+		var restore = this.expose();
+		var result = fn.apply(this);
+		restore();
+		toMeasure.each(function(restore){
+			restore();
+		});
+		return result;
+	},
+
+	expose: function(){
+		if (this.getStyle('display') != 'none') return $empty;
+		var before = this.style.cssText;
+		this.setStyles({
+			display: 'block',
+			position: 'absolute',
+			visibility: 'hidden'
+		});
+		return function(){
+			this.style.cssText = before;
+		}.bind(this);
+	},
+
+	getDimensions: function(options){
+		options = $merge({computeSize: false},options);
+		var dim = {};
+		var getSize = function(el, options){
+			return (options.computeSize)?el.getComputedSize(options):el.getSize();
+		};
+		var parent = this.getParent('body');
+		if (parent && this.getStyle('display') == 'none'){
+			dim = this.measure(function(){
+				return getSize(this, options);
+			});
+		} else if (parent){
+			try { //safari sometimes crashes here, so catch it
+				dim = getSize(this, options);
+			}catch(e){}
+		} else {
+			dim = {x: 0, y: 0};
+		}
+		return $chk(dim.x) ? $extend(dim, {width: dim.x, height: dim.y}) : $extend(dim, {x: dim.width, y: dim.height});
+	},
+
+	getComputedSize: function(options){
+		options = $merge({
+			styles: ['padding','border'],
+			plains: {
+				height: ['top','bottom'],
+				width: ['left','right']
+			},
+			mode: 'both'
+		}, options);
+		var size = {width: 0,height: 0};
+		switch (options.mode){
+			case 'vertical':
+				delete size.width;
+				delete options.plains.width;
+				break;
+			case 'horizontal':
+				delete size.height;
+				delete options.plains.height;
+				break;
+		}
+		var getStyles = [];
+		//this function might be useful in other places; perhaps it should be outside this function?
+		$each(options.plains, function(plain, key){
+			plain.each(function(edge){
+				options.styles.each(function(style){
+					getStyles.push((style == 'border') ? style + '-' + edge + '-' + 'width' : style + '-' + edge);
+				});
+			});
+		});
+		var styles = {};
+		getStyles.each(function(style){ styles[style] = this.getComputedStyle(style); }, this);
+		var subtracted = [];
+		$each(options.plains, function(plain, key){ //keys: width, height, plains: ['left', 'right'], ['top','bottom']
+			var capitalized = key.capitalize();
+			size['total' + capitalized] = size['computed' + capitalized] = 0;
+			plain.each(function(edge){ //top, left, right, bottom
+				size['computed' + edge.capitalize()] = 0;
+				getStyles.each(function(style, i){ //padding, border, etc.
+					//'padding-left'.test('left') size['totalWidth'] = size['width'] + [padding-left]
+					if (style.test(edge)){
+						styles[style] = styles[style].toInt() || 0; //styles['padding-left'] = 5;
+						size['total' + capitalized] = size['total' + capitalized] + styles[style];
+						size['computed' + edge.capitalize()] = size['computed' + edge.capitalize()] + styles[style];
+					}
+					//if width != width (so, padding-left, for instance), then subtract that from the total
+					if (style.test(edge) && key != style &&
+						(style.test('border') || style.test('padding')) && !subtracted.contains(style)){
+						subtracted.push(style);
+						size['computed' + capitalized] = size['computed' + capitalized]-styles[style];
+					}
+				});
+			});
+		});
+
+		['Width', 'Height'].each(function(value){
+			var lower = value.toLowerCase();
+			if(!$chk(size[lower])) return;
+
+			size[lower] = size[lower] + this['offset' + value] + size['computed' + value];
+			size['total' + value] = size[lower] + size['total' + value];
+			delete size['computed' + value];
+		}, this);
+
+		return $extend(styles, size);
+	}
+
+});
+
+/*
+---
+
+script: Element.Position.js
+
+description: Extends the Element native object to include methods useful positioning elements relative to others.
+
+license: MIT-style license
+
+authors:
+- Aaron Newton
+
+requires:
+- core:1.2.4/Element.Dimensions
+- /Element.Measure
+
+provides: [Elements.Position]
+
+...
+*/
+
+(function(){
+
+var original = Element.prototype.position;
+
+Element.implement({
+
+	position: function(options){
+		//call original position if the options are x/y values
+		if (options && ($defined(options.x) || $defined(options.y))) return original ? original.apply(this, arguments) : this;
+		$each(options||{}, function(v, k){ if (!$defined(v)) delete options[k]; });
+		options = $merge({
+			// minimum: { x: 0, y: 0 },
+			// maximum: { x: 0, y: 0},
+			relativeTo: document.body,
+			position: {
+				x: 'center', //left, center, right
+				y: 'center' //top, center, bottom
+			},
+			edge: false,
+			offset: {x: 0, y: 0},
+			returnPos: false,
+			relFixedPosition: false,
+			ignoreMargins: false,
+			ignoreScroll: false,
+			allowNegative: false
+		}, options);
+		//compute the offset of the parent positioned element if this element is in one
+		var parentOffset = {x: 0, y: 0}, 
+				parentPositioned = false;
+		/* dollar around getOffsetParent should not be necessary, but as it does not return
+		 * a mootools extended element in IE, an error occurs on the call to expose. See:
+		 * http://mootools.lighthouseapp.com/projects/2706/tickets/333-element-getoffsetparent-inconsistency-between-ie-and-other-browsers */
+		var offsetParent = this.measure(function(){
+			return document.id(this.getOffsetParent());
+		});
+		if (offsetParent && offsetParent != this.getDocument().body){
+			parentOffset = offsetParent.measure(function(){
+				return this.getPosition();
+			});
+			parentPositioned = offsetParent != document.id(options.relativeTo);
+			options.offset.x = options.offset.x - parentOffset.x;
+			options.offset.y = options.offset.y - parentOffset.y;
+		}
+		//upperRight, bottomRight, centerRight, upperLeft, bottomLeft, centerLeft
+		//topRight, topLeft, centerTop, centerBottom, center
+		var fixValue = function(option){
+			if ($type(option) != 'string') return option;
+			option = option.toLowerCase();
+			var val = {};
+			if (option.test('left')) val.x = 'left';
+			else if (option.test('right')) val.x = 'right';
+			else val.x = 'center';
+			if (option.test('upper') || option.test('top')) val.y = 'top';
+			else if (option.test('bottom')) val.y = 'bottom';
+			else val.y = 'center';
+			return val;
+		};
+		options.edge = fixValue(options.edge);
+		options.position = fixValue(options.position);
+		if (!options.edge){
+			if (options.position.x == 'center' && options.position.y == 'center') options.edge = {x:'center', y:'center'};
+			else options.edge = {x:'left', y:'top'};
+		}
+
+		this.setStyle('position', 'absolute');
+		var rel = document.id(options.relativeTo) || document.body,
+				calc = rel == document.body ? window.getScroll() : rel.getPosition(),
+				top = calc.y, left = calc.x;
+
+		var dim = this.getDimensions({computeSize: true, styles:['padding', 'border','margin']});
+		var pos = {},
+				prefY = options.offset.y,
+				prefX = options.offset.x,
+				winSize = window.getSize();
+		switch(options.position.x){
+			case 'left':
+				pos.x = left + prefX;
+				break;
+			case 'right':
+				pos.x = left + prefX + rel.offsetWidth;
+				break;
+			default: //center
+				pos.x = left + ((rel == document.body ? winSize.x : rel.offsetWidth)/2) + prefX;
+				break;
+		}
+		switch(options.position.y){
+			case 'top':
+				pos.y = top + prefY;
+				break;
+			case 'bottom':
+				pos.y = top + prefY + rel.offsetHeight;
+				break;
+			default: //center
+				pos.y = top + ((rel == document.body ? winSize.y : rel.offsetHeight)/2) + prefY;
+				break;
+		}
+		if (options.edge){
+			var edgeOffset = {};
+
+			switch(options.edge.x){
+				case 'left':
+					edgeOffset.x = 0;
+					break;
+				case 'right':
+					edgeOffset.x = -dim.x-dim.computedRight-dim.computedLeft;
+					break;
+				default: //center
+					edgeOffset.x = -(dim.totalWidth/2);
+					break;
+			}
+			switch(options.edge.y){
+				case 'top':
+					edgeOffset.y = 0;
+					break;
+				case 'bottom':
+					edgeOffset.y = -dim.y-dim.computedTop-dim.computedBottom;
+					break;
+				default: //center
+					edgeOffset.y = -(dim.totalHeight/2);
+					break;
+			}
+			pos.x += edgeOffset.x;
+			pos.y += edgeOffset.y;
+		}
+		pos = {
+			left: ((pos.x >= 0 || parentPositioned || options.allowNegative) ? pos.x : 0).toInt(),
+			top: ((pos.y >= 0 || parentPositioned || options.allowNegative) ? pos.y : 0).toInt()
+		};
+		var xy = {left: 'x', top: 'y'};
+		['minimum', 'maximum'].each(function(minmax) {
+			['left', 'top'].each(function(lr) {
+				var val = options[minmax] ? options[minmax][xy[lr]] : null;
+				if (val != null && pos[lr] < val) pos[lr] = val;
+			});
+		});
+		if (rel.getStyle('position') == 'fixed' || options.relFixedPosition){
+			var winScroll = window.getScroll();
+			pos.top+= winScroll.y;
+			pos.left+= winScroll.x;
+		}
+		if (options.ignoreScroll) {
+			var relScroll = rel.getScroll();
+			pos.top-= relScroll.y;
+			pos.left-= relScroll.x;
+		}
+		if (options.ignoreMargins) {
+			pos.left += (
+				options.edge.x == 'right' ? dim['margin-right'] : 
+				options.edge.x == 'center' ? -dim['margin-left'] + ((dim['margin-right'] + dim['margin-left'])/2) : 
+					- dim['margin-left']
+			);
+			pos.top += (
+				options.edge.y == 'bottom' ? dim['margin-bottom'] : 
+				options.edge.y == 'center' ? -dim['margin-top'] + ((dim['margin-bottom'] + dim['margin-top'])/2) : 
+					- dim['margin-top']
+			);
+		}
+		pos.left = Math.ceil(pos.left);
+		pos.top = Math.ceil(pos.top);
+		if (options.returnPos) return pos;
+		else this.setStyles(pos);
+		return this;
+	}
+
+});
+
+})();
--- smugal/smugal.js
+++ smugal/smugal.js
@@ -6,91 +6,154 @@
  *  @author Schell Scivally
  *  @since  Sun May 30 16:58:26 PDT 2010
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * **/
-var testing = true;
-var galleryModel = {};
-var albumId = '11397881';
-var albumKey = 'MYoeS';
-var maxSizeOfDisplayedImage = 3;
 
-//--------------------------------------
-//  Page elements
-//--------------------------------------
-var mainDiv = new Element('div', {
-    id : 'mainDiv',
-    text : 'loading smug mug gallery...'
-});
-
-function transition (index) {
-    if (! transition.started) {
-        transition.started = true;
-        
-    } else {
-        
-    }
-}
-
-function downloadImages () {
-    var n = galleryModel.images.length;
-    for (var i = 0; i < n; i++) {
-        var image = new Image();
-        image.src = galleryModel.images[i].urls[maxSizeOfDisplayedImage];
-        image.onload = function (index, img) {
-            return function () {
-                var imageModel = galleryModel.images[index];
-                imageModel.image = img;
-                imageModel.element = new Element('img');
-                imageModel.element.inject(mainDiv);
-                //mainDiv.setProperty('html', mainDiv.getProperty('html') + '<br />' + imageModel.image.src);
-                imageModel.tween = new Fx.Tween(imageModel.element, {
-                    property : 'opacity',
-                    duration : 1000
-                });
-                galleryModel.images[index].model = imageModel;
-            }
-        }(i, image);
-    }
-}
-
-function display () {
-    mainDiv.inject(document.body);
-    downloadImages();
-}
-
-function setup (atomFeed) {
-    var mediaGroupsMatch = atomFeed.match(/<media:group>/g);
-    var urlRegex = /<media:content[^>]*url="([^"]*)"/g;
+var smugal = function (options) {
+    // options
+    var testing = options.testing || true;
+    var albumId = options.albumId || '11397881';
+    var albumKey = options.albumKey || 'MYoeS';
+    var maxSizeOfDisplayedImage = options.maxSizeOfDisplayedImage || 3;
+    var viewDuration = options.viewDuration || 2000;
+    var fadeDuration = options.fadeDuration || 1000;
+    // non options
     
-    numImages = mediaGroupsMatch.length;
-    var imageName = '';
-    galleryModel.images = [];
-    while(match = urlRegex.exec(atomFeed)) {
-        var filenameSlash = match[1].lastIndexOf('/');
-        var newImageName = match[1].substring(0, filenameSlash);
-        if (newImageName != imageName) {
-            galleryModel.images.push({
-                name : newImageName,
-                urls : []
-            });
-            imageName = newImageName;
-        }
-        var currentImage = galleryModel.images[galleryModel.images.length - 1];
-        currentImage.urls.push(match[1]);
-    }
-    
-    display();
-}
-
-window.addEvent('domready', function (e) {
-    var feedReq = new Request({
-        method : 'get',
-        url : 'http://smugmuggallery.local/feed.mg',
-        onSuccess : function (text, xml) {
-            setup(text);
-        }, 
-        onFailure : function (xhr) {
-            alert('failed:\n'+xhr);
-        }
+    var galleryModel = {};
+    var tweens = [];
+    var imgStatus = {};
+    //--------------------------------------
+    //  Page elements
+    //--------------------------------------
+    var loadingDiv = new Element('div', {
+        id : 'loadingDiv',
+        text : 'loading smug mug gallery...'
     });
-    var vars = testing ? '' : 'Type=gallery&Data='+albumId+'_'+albumKey;
-    feedReq.send(vars);
-});
+
+    var mainDiv = new Element('div', {
+        id : 'mainDiv'
+    });
+
+    function transition () {
+        var imgs = mainDiv.getElements('img');
+        var out = transition.ndx;
+        var inn = transition.ndx == tweens.length - 1 ? 0 : transition.ndx + 1;
+        var imgOut = $('img_' + out);
+        var inId = 'img_' + inn;
+        var imgIn = $(inId);
+    
+        transition.ndx = inn;
+        if (! transition.started) {
+            transition.started = true;
+            imgIn.setStyle('display', 'inline');
+            imgIn.position();
+            tweens[inn].start('opacity', 0, 1).chain(function () {
+                setTimeout(transition, viewDuration);
+            });
+        } else {
+            if (imgStatus[inId] === 'loading') {
+                imgStatus[inId] = 'waiting';
+                return;
+            }
+            tweens[out].start('opacity', 1, 0).chain(function () {
+                imgOut.setStyle('display', 'none');
+            });
+            imgIn.position();
+            imgIn.setStyle('display', 'inline');
+            tweens[inn].start('opacity', 0, 1).chain(function () {
+                setTimeout(transition, viewDuration);
+            });
+        }
+    }
+    transition.started = false;
+    transition.ndx = -1;
+
+    function downloadImages () {
+        var n = galleryModel.images.length;
+    
+        var anotherImageLoaded = function (thatId) {
+            var id = this.id || thatId;
+            console.log(id);
+            var status = imgStatus[id];
+            imgStatus[id] = 'complete';
+            if (status === 'waiting') {
+                transition();
+            }
+        };
+
+        var firstImageLoaded = function () {
+            anotherImageLoaded(this.id);
+            loadingDiv.dispose();
+            transition();
+        };
+    
+        for (var i = 0; i < n; i++) {
+            var id = 'img_' + i;
+            imgStatus[id] = 'loading';
+            var maxSize = galleryModel.images[i].urls.length - 1;
+            var size = maxSize >= maxSizeOfDisplayedImage ? maxSizeOfDisplayedImage : maxSize;
+            console.log(size);
+            var src = galleryModel.images[i].urls[size];
+            var img = new Element('img', {
+                src : src, 
+                id : id,
+                styles : {
+                    border : '#000000 solid 1px',
+                    display : 'none'
+                },
+                events : {
+                    load : (i == 0 ? firstImageLoaded : anotherImageLoaded)
+                }
+            });
+            var tween = new Fx.Tween(img, {
+                duration : fadeDuration
+            });
+            tween.set('opacity', 0);
+            tweens.push(tween);
+            img.inject(mainDiv);
+        }
+    }
+
+    function display () {
+        loadingDiv.inject(mainDiv);
+        mainDiv.inject(document.body);
+        downloadImages();
+    }
+
+    function setup (atomFeed) {
+        var mediaGroupsMatch = atomFeed.match(/<media:group>/g);
+        var urlRegex = /<media:content[^>]*url="([^"]*)"/g;
+    
+        numImages = mediaGroupsMatch.length;
+        var imageName = '';
+        galleryModel.images = [];
+        while(match = urlRegex.exec(atomFeed)) {
+            var filenameSlash = match[1].lastIndexOf('/');
+            var newImageName = match[1].substring(0, filenameSlash);
+            if (newImageName != imageName) {
+                galleryModel.images.push({
+                    name : newImageName,
+                    urls : []
+                });
+                imageName = newImageName;
+            }
+            var currentImage = galleryModel.images[galleryModel.images.length - 1];
+            currentImage.urls.push(match[1]);
+        }
+    
+        display();
+    }
+
+    window.addEvent('domready', function (e) {
+        var feedReq = new Request({
+            method : 'get',
+            url : 'http://smugmuggallery.local/feed.mg',
+            onSuccess : function (text, xml) {
+                setup(text);
+            }, 
+            onFailure : function (xhr) {
+                alert('failed:\n'+xhr);
+            }
+        });
+        var vars = testing ? '' : 'Type=gallery&Data='+albumId+'_'+albumKey;
+        feedReq.send(vars);
+    });
+}
